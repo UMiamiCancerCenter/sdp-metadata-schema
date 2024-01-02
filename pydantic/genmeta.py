fb7cbbf182cbaa09c2e7de64312fb482be39a1b7
msinclair2 <msinclair2>
2024-01-02 16:15:27 -0500

    Updating field names

--- pydantic/genmeta.py
+++ pydantic/genmeta.py
@@ -13,25 +13,27 @@ from pydantic.config import ConfigDict
 
 class SmallMolecule(BaseModel):
     
-    Name: str
+    model_config = ConfigDict(title="Small Molecule")
+
+    Name: str = Field(title="Small Molecule Name")
     Duration: str
     Concentration: str
 
 class CRISPR(BaseModel):
     
-    Name: str
+    Name: str = Field(title="Target Gene")
     Duration: str
     Concentration: str
 
 class RNAi(BaseModel):
     
-    Name: str
+    Name: str = Field(title="Target Sequence")
     Duration: str
     Concentration: str
 
 class Antibody(BaseModel):
     
-    Name: str
+    Name: str = Field(title="Antibody Name")
     Duration: str
     Concentration: str
 
@@ -39,59 +41,71 @@ class ProteinP(BaseModel):
     
     model_config = ConfigDict(title='Protein')
 
-    Name: str
+    Name: str = Field(title="Protein Name")
     Duration: str
     Concentration: str
 
 class InfectiousAgent(BaseModel):
     
-    Name: str
+    model_config = ConfigDict(title="Infectious Agent")
+
+    Name: str = Field(title="Agent Name")
     Duration: str
     Concentration: str
 
 class EpigeneticModification(BaseModel):
+
+    model_config = ConfigDict(title="Epigenetic Modification")
     
-    Name: str
+    Name: str = Field(title="Modification Type")
 
 class Protein(BaseModel):
     
-    Name: str
+    Name: str = Field(title="Protein Name")
 
 class Transcript(BaseModel):
     
-    Name: str
+    Name: str = Field(title="NCBI Accession Number")
 
 class Gene(BaseModel):
     
-    Name: str
+    Name: str = Field(title="Gene Name")
 
 class Tissue(BaseModel):
     
-    Name: str
+    Name: str = Field(title="Tissue Type")
 
 class PrimaryCells(BaseModel):
+
+    model_config = ConfigDict(title="Primary Cells")
     
-    Name: str
+    Name: str = Field(title="Cell Type")
 
 class DifferentiatedCells(BaseModel):
     
-    Name: str
+    model_config = ConfigDict(title="Differentiated Cells")
+
+    Name: str = Field(title="Cell Type")
 
 class iPSC(BaseModel):
     
-    Name: str
+    Name: str = Field(title="iPSC ID")
 
 class CellLine(BaseModel):
+
+    model_config = ConfigDict(title="Cell Line")
     
-    Name: str
+    Name: str = Field(title="Cell Line Name")
 
 class Wastewater(BaseModel):
     
-    Name: str
+    Name: str = Field(title="Sample ID")
 
 class PatientSample(BaseModel):
     
-    Name: str
+    model_config = ConfigDict(title="Patient Sample")
+
+    Name: str = Field(title="Tumor Type")
 
 class Target(BaseModel):
     
@@ -112,6 +126,8 @@ class Biospecimen(BaseModel):
 
 
 class ModelSystem(BaseModel):
+
+    model_config = ConfigDict(title="Model System")
    
     type: Union[
             CellLine,
@@ -124,13 +140,13 @@ class ModelSystem(BaseModel):
 
 class Sample(BaseModel):
     
-    SampleName: str
+    SampleName: str = Field(title='Sample Name')
     Description: str
     ExperimentalSubject: Union[
             ModelSystem,
             Biospecimen,
             Target
-            ]
+            ] = Field(title="Experimental Subject")
     Perturbation: List[Union[
         SmallMolecule,
         CRISPR,
