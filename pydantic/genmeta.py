6819ab05acaf4266f7a4ddfef4f0a99bbd02e24b
msinclair2 <msinclair2>
2024-01-16 16:48:09 -0500

    Test default empty string for Cell Line Name

--- pydantic/genmeta.py
+++ pydantic/genmeta.py
@@ -101,7 +101,7 @@ class cellLine(BaseModel):
 
     model_config = ConfigDict(title="Cell Line")
     
-    name: str = Field(title="Cell Line Name")
+    name: str = Field(default="", title="Cell Line Name")
     className: str = Field('Cell Line', frozen=True)
 
 class wastewater(BaseModel):
