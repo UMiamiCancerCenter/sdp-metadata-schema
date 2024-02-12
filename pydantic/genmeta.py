ee0f4b237a76f6188a5a83f46372e0f109a613b4
msinclair2 <msinclair2>
2024-02-12 11:24:53 -0500

    datagrid experimentalSystem.content.entity

--- pydantic/genmeta.py
+++ pydantic/genmeta.py
@@ -246,7 +246,7 @@ class sample(BaseModel):
                                         {
                                         "field": "experimentalSystemColumn",
                                         "title": "Experimental System",
-                                        "getCellValue": "experimentalSystem.content"
+                                        "getCellValue": "experimentalSystem.content.entity"
                                         }
                                     ]
                                 }
