6de82cc21f72158b8db63672676bb0464c476ee5
Marcin Pilarczyk <pilarczykm@gmail.com>
2024-06-10 13:16:19 +0200

    Revert "Sdp 2201 import data to pydantic from external sources to create json schemas (#3)"
    This reverts commit e6e65d8e202a3a7985701bbcff7dd0748c2929d7.

--- pydantic/settings.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
-
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env')
-
-    pg_dsn: str = Field(alias="PG_DSN")
-    project_id: int = Field(alias="PROJECT_ID")
