3dc93908a1d76e20ffe963e07d00aed96450936e
msinclair2 <msinclair2>
2024-07-19 02:51:00 -0400

    SDP-2496 Adding biovalidator fields to cell line, CRISPR knockout.

--- pydantic/genmeta.py
+++ pydantic/genmeta.py
@@ -67,11 +67,12 @@ class crisprKnockout(BaseModel):
     crisprKnockoutLabBatchLabel: str = Field(title="Lab Batch Label", 
                                      description="Lab-specific ID for the batch of CRISPR reagent used in the experiment.", default="")
     crisprKnockoutTargetGeneSymbol: str = Field(title="Target Gene Symbol",
-                                         description="The HGNC (human), MGI (mouse), RGD (rat), or ZFIN (zebrafish) symbol of the gene knocked out by CRISPR for knockout, or VGNC gene symbols for other vertebrate species.")
-    crisprKnockoutTargetGeneId: str = Field(title="NCBI Entrez ID for Target Gene", 
-                                    description="The NCBI Entrez Gene ID for the gene knocked out by CRISPR.", default="")
+                                    description="The HGNC (human), MGI (mouse), RGD (rat), or ZFIN (zebrafish) symbol of the gene knocked out by CRISPR for knockout, or VGNC gene symbols for other vertebrate species.")
+    crisprKnockoutTargetGeneId: str = Field(title=
+                                            "NCBI Entrez ID for Target Gene",
+                                            description="The NCBI Entrez Gene ID for the gene knocked out by CRISPR.", default="")
     crisprKnockoutTargetGeneSpecies: str = Field(title="Target Gene Species", 
-                                         description="The species of the target locus, with name chosen from the NCBI Taxonomy. Must be a child term of 'cellular organisms'.")
+                                         description="The species of the target locus, with name chosen from the NCBI Taxonomy. Must be a child term of 'cellular organisms'.",json_schema_extra={"graphRestriction":{"ontologies":["obo:ncbitaxon"],"classes": ["NCBITaxon:131567"],"queryFields": ["label"],"includeSelf": True}})
     crisprKnockoutDuration: float = Field(title="Duration", 
                                          description="Amount of time the experimental system was exposed to the CRISPR reagent.")
     crisprKnockoutDurationUnits: str = Field(title="Duration Units",
@@ -296,22 +297,21 @@ class cellLine(BaseModel):
     cellLineId: str = Field(default="", json_schema_extra={"format": "hidden"})
 
     cellLineName: str = Field(title="Name", 
-                              description="The cell line name as found in the Cell Line Ontology. Must be a child term of 'immortal cell line cell'." 
+                              description="The cell line name as found in the Cell Line Ontology. Must be a child term of 'immortal cell line cell'.", json_schema_extra={"graphRestriction":  {"ontologies": ["obo:clo"],"classes": ["CLO:0000019"], "queryFields": ["label"], "includeSelf": True}} 
                               )
     cellLineLabBatchLabel: str = Field(title="Lab Batch Label", 
                                        description="Lab-specific ID for the  batch of cells used in the experiment.", default="")
     cellLineTissue: str = Field(title="Tissue of Origin", 
-                                description="Tissue from which the cell line was derived, with name chosen from NCI Thesaurus, Brenda Tissue Ontology, or UBERON. Must be a child term of 'Tissue (NCIT)', 'tissues, cell types, and enzyme sources (BTO), or tissue (UBERON)'."
+                                description="Tissue from which the cell line was derived, with name chosen from NCI Thesaurus, Brenda Tissue Ontology, or UBERON. Must be a child term of 'Tissue (NCIT)', 'tissues, cell types, and enzyme sources (BTO), or tissue (UBERON)'.", json_schema_extra={"graphRestriction":  {"ontologies" : ["obo:ncit","obo:bto","obo:uberon"], "classes": ["NCIT:C12801","BTO:0000000","UBERON:0000479"], "queryFields": ["label"], "includeSelf": True}}
                                 )
     cellLineOrgan: str = Field(title="Organ of Origin", 
-                               description="Organ from which the cell line was derived, with name chosen from NCI Thesaurus, UBERON, or FMA. Must be a child term of 'organ'.",
-                               default="")
+                               description="Organ from which the cell line was derived, with name chosen from NCI Thesaurus, UBERON, or FMA. Must be a child term of 'organ'.",default="",json_schema_extra={"graphRestriction":  {"ontologies":["obo:ncit","obo:fma","obo:uberon"],"classes": ["NCIT:C13018","FMA:67498","UBERON:0000062"],"queryFields": ["label"],"includeSelf": True}})
     cellLineSpecies: str = Field(title="Species of Origin", 
-                                  description="Species from which the cell line was derived, with name chosen from the NCBI Taxonomy. Must be a child term of 'cellular organisms'."
-                                  )
+                                  description="Species from which the cell line was derived, with name chosen from the NCBI Taxonomy. Must be a child term of 'cellular organisms'.",json_schema_extra={"graphRestriction":  {"ontologies":["obo:ncbitaxon"],"classes": ["NCBITaxon:131567"],"queryFields": ["label"],"includeSelf": True}})
     cellLineDisease: str = Field(title="Disease", 
                                  description="If the cell line came from a diseased tissue, the disease name must be taken from the Disease Ontology. Must be a child term of 'disease'. Leave blank if the origin tissue or cells were not diseased.",
-                                 default="")
+                                 default="",json_schema_extra={"graphRestriction":  {"ontologies": ["obo:doid"],"classes": ["DOID:4"],"queryFields": ["label"],"includeSelf": True
+                                }})
 
 
 
