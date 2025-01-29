dfcb374fbb3692d7eae0af4f61aa8f4c23525ad8
msinclair2 <msinclair2>
2025-01-29 14:06:20 -0500

    v0.0.17

--- json_schemas/sample/sample.py
+++ json_schemas/sample/sample.py
@@ -500,7 +500,7 @@ class tissue(BaseModel):
 class sample(BaseModel):
 
     model_config = ConfigDict(title="Sample", json_schema_extra={
-                        "version": "0.0.15"
+                        "version": "0.0.17"
             })
     
     name: str = Field(title='Sample Name', 
