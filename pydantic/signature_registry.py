f673f3eeeccf867bc5c7d785178df98c25239a15
msinclair2 <msinclair2>
2025-05-02 14:13:49 -0400

    SDP-3897 General Signature Schema

---
+++ pydantic/signature_registry.py
@@ -0,0 +1,84 @@
+import json
+from datetime import datetime, timezone
+from typing import Any, Literal
+
+from utils import (
+    CustomBaseModel,
+    GenerateJsonSchemaWithoutDefaultTitles,
+    ModelSystemType,
+    MongoDate,
+    PerturbationType,
+    PyObjectId,
+    Scope,
+    SignatureType,
+    delete_empty_default,
+)
+
+from pydantic import Field, model_validator
+from pydantic.json_schema import SkipJsonSchema
+
+
+class Perturbation(CustomBaseModel):
+
+    name: str | SkipJsonSchema[None] = Field(default=None, title="Name")
+    perturbation_id: PyObjectId | SkipJsonSchema[None] = Field(default_factory=PyObjectId, title="ID", alias="perturbationId")
+    type: PerturbationType = Field(title="Perturbation Type")
+    concentration: float | SkipJsonSchema[None] = Field(default=None, title="Concentration")
+    concentration_units: str | SkipJsonSchema[None] = Field(default=None, title="Concentration Units", alias="concentrationUnits")
+    duration: float | SkipJsonSchema[None] = Field(default=None, title="Duration")
+    duration_units: str | SkipJsonSchema[None] = Field(default=None, title="Duration Units", alias="durationUnits")
+
+class ModelSystem(CustomBaseModel):
+
+    name: str | SkipJsonSchema[None] = Field(default=None, title="Name")
+    type: ModelSystemType = Field(title="Type")
+    id: PyObjectId | SkipJsonSchema[None] = Field(default=None, title="ID")
+
+class Sample(CustomBaseModel):
+
+    name: str = Field(title="Sample Name", description="Name for this sample.")
+    description: str | SkipJsonSchema[None] = Field(default=None)
+    id: PyObjectId = Field(title="Sample ID", description="ID by which this sample is identified in the input data.")
+    dataset: str | PyObjectId | SkipJsonSchema[None] = Field(default=None, title="Dataset Name or ID")
+
+class SampleGroup(CustomBaseModel):
+    name: str | SkipJsonSchema[None] = Field(default=None, title="Group Name", description="Name for this group of samples.")
+    description: str | SkipJsonSchema[None] = Field(default=None)
+    samples: list[Sample] | SkipJsonSchema[None] = Field(default=None, title="Samples")
+    model_systems: list[ModelSystem] | SkipJsonSchema[None] = Field(default=None, title="Model Systems", alias="modelSystems")
+    perturbations: list[Perturbation] | SkipJsonSchema[None] = Field(default=None, title="Perturbations")
+
+class Analytes(CustomBaseModel):
+    type: str | SkipJsonSchema[None]= Field(default=None, title="Type")
+    items: tuple[str, ...] | PyObjectId | SkipJsonSchema[None] = Field(default=None, title="Analyte(s)")
+
+class Signature(CustomBaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, title="Signature ID")
+    name: str = Field(title="Name")
+    scope: Scope = Field(default=Scope.PRIVATE, alias="scope")
+    signature_type: SignatureType = Field(title="Signature Type", alias="signatureType")
+    description: str | SkipJsonSchema[None] = Field(default=None, title="Description")
+    source: str | SkipJsonSchema[None] = Field(default=None, title="Source")
+    source_link: str | SkipJsonSchema[None] = Field(default=None, title="Source Link", alias="sourceLink")
+    project: str | SkipJsonSchema[None] = Field(default=None, title="Project")
+    organization: str | SkipJsonSchema[None] = Field(default=None, title="Organization")
+    sample_groups: list[SampleGroup] | SkipJsonSchema[None] = Field(default=None, title="Input", alias="sampleGroups")
+    area_of_study: str | SkipJsonSchema[None] = Field(default=None, title="Area of Study", alias="areaOfStudy")
+    assay: str | SkipJsonSchema[None] = Field(default=None, title="Assay")
+    analytes: list[Analytes] = Field(title="Analyte(s)")
+    method: str = Field(title="Processing Method")
+    endpoints: list[str] = Field(title="Endpoint(s)")
+    created_at: MongoDate | SkipJsonSchema[None] = Field(default_factory=lambda: datetime.now(timezone.utc), title="Created At", alias="createdAt")  # noqa: UP017
+
+    @model_validator(mode="before")
+    @classmethod
+    def rename_id(cls, values: dict[str, Any]) -> dict[str, Any]:
+        if "_id" in values:
+            values["id"] = values.pop("_id")
+        return values
+
+if __name__ == "__main__":
+    json_schema=Signature.model_json_schema(schema_generator=GenerateJsonSchemaWithoutDefaultTitles)
+    delete_empty_default(json_schema)
+    with open ("json_schemas/registry/signature/signature.json", "w") as ft:
+        print(json.dumps(json_schema, indent=2), file = ft)
