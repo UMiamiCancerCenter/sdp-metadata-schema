b6ee9e306400a81d4145b8c890165cbe67e566ed
msinclair2 <20292982+msinclair2@users.noreply.github.com>
2025-05-13 13:41:23 -0400

    SDP-3897 General Signature Schema (#20)
    * first commit
    
    * made alternativeNames and alternativeIds a list[str]
    
    * SDP-3897 General Signature Schema
    
    * fixed version number
    
    * SDP-4037 Gene Registry Schema
    
    * changed id to sampleId for samples
    
    * SDP-3897-SDP-4037 changed id to sampleId for Sample
    
    * added modelsystemitem
    
    * SDP-3987-SDP-4037
    
    * updated gene schema
    
    * SDP-4037 updated gene schema
    
    * SDP-4037 update gene schema, change id fieldnames to descriptor+id
    
    * SDP-4037 update gene schema
    
    * SDP-4037 added alias, summary, mapLocation, and wikipedia to Gene schema
    
    * SDP-3987 add data field to signature schema
    
    ---------
    
    Co-authored-by: msinclair2 <msinclair2>

---
+++ pydantic/gene_registry.py
@@ -0,0 +1,83 @@
+import json
+from typing import Any
+
+from utils import (
+    CustomBaseModel,
+    GenerateJsonSchemaWithoutDefaultTitles,
+    Scope,
+    delete_empty_default,
+)
+
+from pydantic import ConfigDict, Field, model_validator
+from pydantic.json_schema import SkipJsonSchema
+
+
+class GoEntry(CustomBaseModel):
+    evidence: str | SkipJsonSchema[None] = Field(default=None, title="Evidence")
+    gocategory: str | SkipJsonSchema[None] = Field(default=None, title="GO Category")
+    go_id: str | SkipJsonSchema[None] = Field(default=None, title="GO ID", alias="goId")
+    qualifier: str | SkipJsonSchema[None] = Field(default=None, title="Qualifier")
+    term: str | SkipJsonSchema[None] = Field(default=None, title="Term")
+
+class GO(CustomBaseModel):
+    BP: list[GoEntry] | SkipJsonSchema[None] = Field(default=None, title="Biological Process")
+    CC: list[GoEntry] | SkipJsonSchema[None] = Field(default=None, title="Cellular Component")
+    MF: list[GoEntry] | SkipJsonSchema[None] = Field(default=None, title="Molecular Function")
+
+class LinkedTrait(CustomBaseModel):
+    label: str | SkipJsonSchema[None] = Field(default=None, title="Label")
+    trait_id: str | SkipJsonSchema[None] = Field(default=None, title="Trait ID", alias="traitId")
+    url: str | SkipJsonSchema[None] = Field(default=None, title="URL")
+
+class GWASAssociation(CustomBaseModel):
+    rs_id: str | SkipJsonSchema[None] = Field(default=None, title="RefSeq ID", alias="rsId")
+    risk_label: str | SkipJsonSchema[None] = Field(default=None, title="Risk Label", alias="riskLabel")
+    p_value: int | SkipJsonSchema[None] = Field(default=None, title="p-Value", alias="pValue")
+    p_value_exponent: int | SkipJsonSchema[None] = Field(default=None, title="p-Value Exponent", alias="pValueExponent")
+    trait_name: str | SkipJsonSchema[None] = Field(default=None, title="Trait Name", alias="traitName")
+    linked_traits: list[LinkedTrait] | SkipJsonSchema[None] = Field(default=None, title="Linked Traits", alias="linkedTraits")
+
+class KeggEntry(CustomBaseModel):
+    kegg_id: str | SkipJsonSchema[None] = Field(default=None, title="KEGG ID", alias="keggId")
+    kegg_name: str | SkipJsonSchema[None] = Field(default=None, title="Name", alias="keggName")
+
+class ReactomeEntry(CustomBaseModel):
+    reactome_id: str | SkipJsonSchema[None] = Field(default=None, title="Reactome ID", alias="reactomeId")
+    reactome_name: str | SkipJsonSchema[None] = Field(default=None, title="Name", alias="reactomeName")
+
+class WikipediaStub(CustomBaseModel):
+    url_stub: str | SkipJsonSchema[None] = Field(default=None, title="URL Stub", alias="urlStub")
+class Gene(CustomBaseModel):
+    model_config = ConfigDict(title="Gene Registry", json_schema_extra={"version": "0.0.26"})
+
+    name: str = Field(title="Name")
+    scope: Scope = Field(default=Scope.PRIVATE, title="Scope")
+    summary: str | SkipJsonSchema[None] = Field(default=None, title="Summary")
+    alias: list[str] | SkipJsonSchema[None] = Field(default=None, title="Aliases")
+    HGNC: str | SkipJsonSchema[None] = Field(default=None)
+    symbol: str | SkipJsonSchema[None] = Field(default=None, title="Gene Symbol")
+    map_location: str | SkipJsonSchema[None] = Field(default=None, title="Map Location", alias="mapLocation")
+    entrez_gene: str | SkipJsonSchema[None] = Field(default=None, title="Entrez Gene ID", alias="entrezGene")
+    ensembl: str | SkipJsonSchema[None] = Field(default=None, title="Ensembl ID")
+    other_names: list[str] | SkipJsonSchema[None] = Field(default=None, title="Other Names", alias="otherNames")
+    taxid: int | SkipJsonSchema[None] = Field(default=None, title="Taxonomy ID")
+    species: str = Field(title="Species")
+    type_of_gene: str | SkipJsonSchema[None] = Field(default=None, title="Type of Gene", alias="typeOfGene")
+    go: GO | SkipJsonSchema[None] = Field(default=None, title="Gene Ontology")
+    kegg: list[KeggEntry] | SkipJsonSchema[None] = Field(default=None, title="KEGG")
+    reactome: list[ReactomeEntry] | SkipJsonSchema[None] = Field(default=None, title="Reactome")
+    gwas_associations: list[GWASAssociation] | SkipJsonSchema[None] = Field(default=None, title="GWAS Asssociations", alias="gwasAssociations")
+    wikipedia: WikipediaStub | SkipJsonSchema[None] = Field(default=None, title="Wikipedia Reference")
+
+    @model_validator(mode="before")
+    @classmethod
+    def rename_id(cls, values: dict[str, Any]) -> dict[str, Any]:
+        if "_id" in values:
+            values["id"] = values.pop("_id")
+        return values
+
+if __name__ == "__main__":
+    json_schema=Gene.model_json_schema(schema_generator=GenerateJsonSchemaWithoutDefaultTitles)
+    delete_empty_default(json_schema)
+    with open ("json_schemas/registry/gene/gene.json", "w") as ft:
+        print(json.dumps(json_schema, indent=2), file = ft)
---
+++ pydantic/signature_registry.py
@@ -0,0 +1,84 @@
+import json
+from typing import Any
+
+from utils import (
+    CustomBaseModel,
+    GenerateJsonSchemaWithoutDefaultTitles,
+    PerturbationType,
+    Scope,
+    SignatureType,
+    delete_empty_default,
+)
+
+from pydantic import ConfigDict, Field, model_validator
+from pydantic.json_schema import SkipJsonSchema
+
+
+class Perturbation(CustomBaseModel):
+
+    name: str | SkipJsonSchema[None] = Field(default=None, title="Name")
+    perturbation_id: str | SkipJsonSchema[None] = Field(default=None, title="ID", alias="perturbationId", json_schema_extra={"pattern": "^[a-fA-F0-9]{24}$"})
+    type: PerturbationType = Field(title="Perturbation Type")
+    concentration: float | SkipJsonSchema[None] = Field(default=None, title="Concentration")
+    concentration_units: str | SkipJsonSchema[None] = Field(default=None, title="Concentration Units", alias="concentrationUnits")
+    duration: float | SkipJsonSchema[None] = Field(default=None, title="Duration")
+    duration_units: str | SkipJsonSchema[None] = Field(default=None, title="Duration Units", alias="durationUnits")
+
+class ModelSystemItem(CustomBaseModel):
+    name: str | SkipJsonSchema[None] = Field(default=None, title="Name")
+    model_system_id: str | SkipJsonSchema[None] = Field(default=None, title="ID", alias="modelSystemId", json_schema_extra={"pattern": "^[a-fA-F0-9]{24}$"})
+
+class ModelSystem(CustomBaseModel):
+
+    name: str | SkipJsonSchema[None] = Field(default=None, title="Name")
+    items: list[ModelSystemItem] | SkipJsonSchema[None] = Field(default=None, title="Model System Items")
+
+class Sample(CustomBaseModel):
+
+    name: str = Field(title="Sample Name", description="Name for this sample.")
+    description: str | SkipJsonSchema[None] = Field(default=None)
+    sample_id: str | SkipJsonSchema[None] = Field(default=None, title="Sample ID", description="ID by which this sample is identified in the input data.", alias="sampleId", json_schema_extra={"pattern": "^[a-fA-F0-9]{24}$"})
+    dataset: str | SkipJsonSchema[None] = Field(default=None, title="Dataset Name or ID")
+
+class SampleGroup(CustomBaseModel):
+    name: str | SkipJsonSchema[None] = Field(default=None, title="Group Name", description="Name for this group of samples.")
+    description: str | SkipJsonSchema[None] = Field(default=None)
+    samples: list[Sample] | SkipJsonSchema[None] = Field(default=None, title="Samples")
+    model_systems: list[ModelSystem] | SkipJsonSchema[None] = Field(default=None, title="Model Systems", alias="modelSystems")
+    perturbations: list[Perturbation] | SkipJsonSchema[None] = Field(default=None, title="Perturbations")
+
+class Analytes(CustomBaseModel):
+    type: str | SkipJsonSchema[None]= Field(default=None, title="Type")
+    items: tuple[str, ...] | str | SkipJsonSchema[None] = Field(default=None, title="Analyte(s)", description="List of analyte names, or a single ObjectId string pointing to the list.")
+
+class Signature(CustomBaseModel):
+    model_config = ConfigDict(title="Signature", json_schema_extra={"version": "0.0.26"})
+
+    name: str = Field(title="Name")
+    scope: Scope = Field(default=Scope.PRIVATE, title="Scope", alias="scope")
+    signature_type: SignatureType = Field(title="Signature Type", alias="signatureType")
+    description: str | SkipJsonSchema[None] = Field(default=None, title="Description")
+    source: str | SkipJsonSchema[None] = Field(default=None, title="Source")
+    source_link: str | SkipJsonSchema[None] = Field(default=None, title="Source Link", alias="sourceLink")
+    project: str | SkipJsonSchema[None] = Field(default=None, title="Project")
+    organization: str | SkipJsonSchema[None] = Field(default=None, title="Organization")
+    sample_groups: list[SampleGroup] | SkipJsonSchema[None] = Field(default=None, title="Input", alias="sampleGroups")
+    area_of_study: str | SkipJsonSchema[None] = Field(default=None, title="Area of Study", alias="areaOfStudy")
+    assay: str | SkipJsonSchema[None] = Field(default=None, title="Assay")
+    analytes: list[Analytes] = Field(title="Analyte(s)")
+    method: str = Field(title="Processing Method")
+    endpoints: list[str] = Field(title="Endpoint(s)")
+    data: list[Any] | SkipJsonSchema[None] = Field(default=None, title="Data")
+
+    @model_validator(mode="before")
+    @classmethod
+    def rename_id(cls, values: dict[str, Any]) -> dict[str, Any]:
+        if "_id" in values:
+            values["id"] = values.pop("_id")
+        return values
+
+if __name__ == "__main__":
+    json_schema=Signature.model_json_schema(schema_generator=GenerateJsonSchemaWithoutDefaultTitles)
+    delete_empty_default(json_schema)
+    with open ("json_schemas/signature/signature.json", "w") as ft:
+        print(json.dumps(json_schema, indent=2), file = ft)
