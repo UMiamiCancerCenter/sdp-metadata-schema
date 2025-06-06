fb807c76ef50725ef2a3b6823ecd05dc4c9f99b3
MateuszDev96 <33595932+MateuszDev96@users.noreply.github.com>
2025-06-06 10:45:34 +0200

    SDP-3747 - prepare before final release

--- pydantic/cell_line_batch.py
+++ pydantic/cell_line_batch.py
@@ -89,7 +89,7 @@ class CellLine(BaseModel):
 
     model_config = ConfigDict(title="Cell Line Batch", 
                               description="Immortalized (naturally or engineered), genetically uniform tissue cells able to reproduce indefinitely in standard culture conditions.", json_schema_extra={
-                        "version": "0.0.27"}
+                        "version": "0.0.28"}
                               )
     
     name: str = Field(title="Cell Line Batch Name", 
