1b166e8f08f1e48276e00175ec5915eff65ab003
msinclair2 <msinclair2>
2025-07-10 09:13:56 -0400

    bump version number to 0.0.34

--- pydantic/sample.py
+++ pydantic/sample.py
@@ -498,7 +498,7 @@ class tissue(BaseModel):
 class sample(BaseModel):
 
     model_config = ConfigDict(title="Sample", json_schema_extra={
-                        "version": "0.0.32"
+                        "version": "0.0.34"
             })
     
     name: str = Field(title='Sample Name', 
